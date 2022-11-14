a408066311e3da173d908f9430b721c056bb9dac
Samuel Majerus <ladolego@gmail.com>
2022-11-14 11:33:35 -0800

    Removes unneeded files.  [End of code-along work for Lesson 6 (see link).] [https://www.learnhowtoprogram.com/react/react-fundamentals/introduction-to-jsx]

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+function App(){
+  const name = "Thato";
+  const name2 = "Haley";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>Help Queue</h1>
+      <h3>3a</h3>
+      <h3>{name} and {name2}</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      {/* This is a JSX comment. */}
+      <hr/>
+    </React.Fragment>
   );
 }
 
