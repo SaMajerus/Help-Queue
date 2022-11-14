94b58767c08752547aac727426ad123437421849
Samuel Majerus <ladolego@gmail.com>
2022-11-14 11:59:52 -0800

    Adds 'components' subdirectory to 'src' directory, for storing all React components.   Adds a couple new components, and updates existing ones accordingly.   Removes another unneeded file. End of code-along work for Lsn 8 (link below). [https://www.learnhowtoprogram.com/react/react-fundamentals/building-a-static-site]

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,18 +1,16 @@
 import React from "react";
 
-function App(){
+function Ticket(){
   const name = "Thato";
   const name2 = "Haley";
   return (
     <React.Fragment>
-      <h1>Help Queue</h1>
       <h3>3a</h3>
       <h3>{name} and {name2}</h3>
       <p><em>Firebase entries not saving!</em></p>
-      {/* This is a JSX comment. */}
       <hr/>
     </React.Fragment>
   );
 }
 
-export default App;
+export default Ticket;
---
+++ src/components/TicketList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Ticket from "./Ticket";
+
+function TicketList(){
+  return (
+    <Ticket />
+  );
+}
+
+export default TicketList;
