38014ae9c1681abb70fdd4261874b55ebe8033dd
Samuel Majerus <ladolego@gmail.com>
2022-11-14 14:42:27 -0800

    Adds 'img' subdirectory, adds code for rendering the image to 'Header'. [End of code-along for Lsn 14.]

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from "react";
+import ticketsImage from "./../img/tickets.png";
 
 function Header(){
   return (
-    <h1>Help Queue</h1>
+    <React.Fragment>
+      <h1>Help Queue</h1>
+      <img src={ticketsImage} alt="An image of tickets" />
+    </React.Fragment>
   );
 }
 
