d1a4bfefea649fc03f4e1a3813e676c4d07c3201
Samuel Majerus <ladolego@gmail.com>
2022-11-16 14:05:54 -0800

    Passes down new Props from TicketControl to TicketList component.  Adds PropTypes to 'TicketList.js' [Lsn 29, Step 2]

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,29 +1,30 @@
 import React from "react";
 import Ticket from "./Ticket";
+import PropTypes from "prop-types";
 
-const mainTicketList = [
-  {
-    names: 'Thato and Haley',
-    location: '3A',
-    issue: 'Firebase won\'t save record. Halp.'
-  },
-  {
-    names: 'Sleater and Kinney',
-    location: '4B',
-    issue: 'Prop types are throwing an error.'
-  },
-  {
-    names: 'Imani & Jacob',
-    location: '9F',
-    issue: 'Child component isn\'t rendering.'
-  }
-];
+// const mainTicketList = [
+//   {
+//     names: 'Thato and Haley',
+//     location: '3A',
+//     issue: 'Firebase won\'t save record. Halp.'
+//   },
+//   {
+//     names: 'Sleater and Kinney',
+//     location: '4B',
+//     issue: 'Prop types are throwing an error.'
+//   },
+//   {
+//     names: 'Imani & Jacob',
+//     location: '9F',
+//     issue: 'Child component isn\'t rendering.'
+//   }
+// ];
 
 function TicketList(){
   return (
     <React.Fragment>
       <hr/>
-      {mainTicketList.map((ticket, index) =>
+      {props.ticketList.map((ticket, index) =>  //Loop thru the list passed down from 'TicketControl'. 
         <Ticket names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
@@ -32,5 +33,10 @@ function TicketList(){
     </React.Fragment>
   );
 }
+
+//Add propTypes for ticketList 
+TicketList.PropTypes = {
+  ticketList: PropTypes.array
+};
 
 export default TicketList;
