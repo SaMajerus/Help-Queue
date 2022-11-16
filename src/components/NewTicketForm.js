71692c74e6394cf07de17e029ac1a9df0573464e
Samuel Majerus <ladolego@gmail.com>
2022-11-16 11:00:44 -0800

    Adds a 'New Ticket' form.   [End of code-along work for Lsn 27 (see link).] [https://www.learnhowtoprogram.com/react/react-fundamentals/adding-a-form]

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,9 +1,30 @@
 import React from "react";
 
-function NewTicketForm(props){
+{/* function NewTicketForm(props){ */}
+function NewTicketForm() {
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    console.log(event.target.names.value);
+    console.log(event.target.location.value);
+    console.log(event.target.issue.value);
+  }
+  
   return (
     <React.Fragment>
-      <h3>This is a form.</h3>
+      <form onSubmit={handleNewTicketFormSubmission}>
+        <input
+          type='text'
+          name='names'
+          placeholder='Pair Names' />
+        <input
+          type='text'
+          name='location'
+          placeholder='Location' />
+        <textarea
+          name='issue'
+          placeholder='Describe your issue.' />
+        <button type='submit'>Help!</button>
+      </form>
     </React.Fragment>
   );
 }
