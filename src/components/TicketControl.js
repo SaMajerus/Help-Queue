bd24e21c8fb1a1af6807465dc4f097efe7621d2b
Samuel Majerus <ladolego@gmail.com>
2022-11-15 16:52:47 -0800

    Adds conditional rendering functionality.   [End of code-along work for Lsn 22 (see link).] [https://www.learnhowtoprogram.com/react/react-fundamentals/conditional-rendering]

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -12,12 +12,20 @@ class TicketControl extends React.Component {
   }
 
   render(){
+    let currentlyVisibleState = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTicketForm />
+    } else {
+      currentlyVisibleState = <TicketList />
+    }
     return (
       <React.Fragment>
+        {currentlyVisibleState}
       </React.Fragment>
     );
   }
 
 }
+
 
 export default TicketControl;
