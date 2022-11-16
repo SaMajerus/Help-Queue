609825ad843f34d6fb5009c1a73b6cedd5a448ff
Samuel Majerus <ladolego@gmail.com>
2022-11-16 13:59:11 -0800

    Adds 'mainTicketList' to State as a property. [Lsn 29, Step 1]

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -7,7 +7,8 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false, 
+      mainTicketList: []
     };  
   }
 
@@ -24,7 +25,7 @@ class TicketControl extends React.Component {
       currentlyVisibleState = <NewTicketForm />;
       buttonText = "Return to Ticket List"; 
     } else {
-      currentlyVisibleState = <TicketList />;
+      currentlyVisibleState = <TicketList ticketList = {this.state.mainTicketList} />;
       buttonText = "Add Ticket"; 
     }
     return (
