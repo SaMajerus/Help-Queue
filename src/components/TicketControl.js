76a6ab107583423128b5d33479bea45bba4db9c7
Samuel Majerus <ladolego@gmail.com>
2022-11-15 16:45:03 -0800

    Adds components and functionality for Local State(s).   [End of code-along work for Lsn 21 (see link).] [https://www.learnhowtoprogram.com/react/react-fundamentals/adding-local-state]

---
+++ src/components/TicketControl.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import NewTicketForm from './NewTicketForm';
+import TicketList from './TicketList';
+
+class TicketControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default TicketControl;
